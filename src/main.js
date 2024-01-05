d7e4b95d0159361511ebe9fba6c63f4cd4f96f10
Lorucino <laforga@gmail.com>
2024-01-05 14:09:01 +0100

    add functionality

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const user = document.querySelector('#user');
+const coin = document.querySelector('#coin');
+const button = document.querySelector('#button');
+
+boton.addEventListener("click", function(e) {
+    coin.innerHTML = "pulsado"
+    nombre = user.value;
+    fetch('https://server.duinocoin.com/balances.json')
+        .then(result => result.json())
+        .then((output) => {
+          console.log('Output: ', output);
+          coin.innerHTML = `${nombre}: ${output[nombre]}`;
+        })
+        .catch(err => console.error(err));
+})
