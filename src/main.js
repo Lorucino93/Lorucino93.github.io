937639f161d5cc710b87ee52f6eb8312dc7a743d
Lorucino <71597742+Lorucino93@users.noreply.github.com>
2024-01-09 14:40:50 +0100

    disabled button

--- src/main.js
+++ src/main.js
@@ -2,21 +2,24 @@ const user = document.querySelector('#user');
 const coin = document.querySelector('#coin');
 const button = document.querySelector('#button');
 
-button.addEventListener("click", function(e) {
-    coin.innerHTML = "wait"
-    nombre = user.value.toLowerCase();
-    fetch('https://server.duinocoin.com/balances.json')
-        .then(result => result.json())
-        .then((output) => {
-          console.log('Output: ', output);
-          coin.innerHTML = `${nombre}: ${output[nombre]}`;
-        })
-        .catch(err => {
-          console.error(err);
-          coin.innerHTML = "error";
-        });
-})
+const balance = null;
 
-// function changeToTwoDecimalFloat(number) {
-//   return number.parseToFloat();
-// }
+getBalances();
+
+button.addEventListener("click", (e) => {
+  coin.innerText = balance[user.innerText]
+});
+
+function getBalances() {
+  const callBackPromise = fetch("https://server.duinocoin.com/balances.json");
+  callBackPromise.then((result) => result.json())
+    .then((output) => {
+      console.log("Output: ", output);
+      balance = output;
+      button.disabled = false;
+    })
+    .catch((err) => {
+      console.error(err);
+      coin.innerHTML = "error";
+    });
+}
